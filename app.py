f95d49613eca0713fc47cb3965767ee13e4bc4f3
Daniel Spoor <142638330+Dmail100@users.noreply.github.com>
2025-05-14 17:03:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.secret_key = "replace-with-a-secure-random-key"
 tasks = []
 
-
 basedir = os.path.abspath(os.path.dirname(__file__)) # Get the directory of the current file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:HybridPower.246@my-db.cabieyu4wy2m.us-east-1.rds.amazonaws.com/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -71,17 +70,21 @@ def register():
             return redirect(url_for('login'))
     return render_template('auth.html', action='Register')
 
-@app.route('/login', methods=['GET','POST'])
+app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        print(f"Login attempt: Username={username}, Password={password}")
         user = User.query.filter_by(username=username).first()
+        print(f"User found: {user}")
         if user is None or not user.check_password(password):
             flash('Invalid credentials.')
+            print("Login failed: Invalid credentials.")
         else:
             session.clear()
             session['user_id'] = user.id
+            print(f"Login successful: User ID={user.id}")
             return redirect(url_for('home'))
     return render_template('auth.html', action='Log In')
 
